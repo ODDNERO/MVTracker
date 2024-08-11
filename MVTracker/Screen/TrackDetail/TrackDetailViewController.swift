a4300b4ce0016cefb5456afe4428bbf1a4ae8d9e
NERO <mxzxro@gmail.com>
2024-08-12 01:01:21 +0900

    🚧 [TrackDetailVC] 스크롤 위치에 따른 NavigationBar 숨김 처리 구현 중

--- MVTracker/Screen/TrackDetail/TrackDetailViewController.swift
+++ MVTracker/Screen/TrackDetail/TrackDetailViewController.swift
@@ -26,17 +26,31 @@ final class TrackDetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         configrueView()
+//        webview.scrollView.delegate = self
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationItem.title = selectedTrackTitle
+//        navigationController?.setNavigationBarHidden(false, animated: true)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 }
+
+//MARK: - WebView 스크롤 최상단일 때만 NavigationBar 띄우기 구현 중
+//extension TrackDetailViewController: UIScrollViewDelegate {
+//    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+//        print("scrollViewDidScroll: \(scrollView.contentOffset.y)")
+//        let shouldHidden = scrollView.contentOffset.y > 0 //MARK: 로딩되면서 중간에 스크롤이 튀는 현상
+//        print("shouldHidden: \(shouldHidden)")
+//        if navigationController?.isNavigationBarHidden != shouldHidden {
+//            navigationController?.setNavigationBarHidden(shouldHidden, animated: true)
+//        }
+//    }
+//}
 
 extension TrackDetailViewController {
     private func configrueView() {
