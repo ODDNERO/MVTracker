3afb6e55c2a94d6026723d3a342d3080e5a9c618
NERO <mxzxro@gmail.com>
2024-08-11 17:32:13 +0900

    ✨ [Music] Response Model

---
+++ MVTracker/Network/Music.swift
@@ -0,0 +1,24 @@
+//
+//  Music.swift
+//  MVTracker
+//
+//  Created by NERO on 8/8/24.
+//
+
+import Foundation
+
+struct Music: Decodable {
+    let resultCount: Int
+    let results: [MusicInfo]
+}
+
+struct MusicInfo: Decodable {
+    let artistID, trackID: Int
+    let artistName, trackName: String
+    let artistViewUrl, trackViewUrl: String
+    let previewUrl: String //MV .m4v
+    let artworkUrl100: String
+    let releaseDate: Date
+    let trackTimeMillis: Int
+    let primaryGenreName: String
+}
